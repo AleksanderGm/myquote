a89aa6a36352a05f70ba3a174907ac49588bfc27
AleksanderGm <aleksgrimo@hotmail.com>
2022-01-28 21:33:30 +0100

    pakke-myquoteRSC

---
+++ myquote.go
@@ -0,0 +1,21 @@
+package myquote
+
+import (
+	"rsc.io/quote"
+)
+
+func Myquote() {
+	return quote.Glass()
+}
+
+func Memory() {
+	return quote.Go()
+}
+
+func Hei() {
+	return quote.Hello()
+}
+
+func Wisdown() {
+	return quote.Opt()
+}
